a3b918bb33634510adc40d837f3d3d8bc263af95
Tafnudo <165534660+Tafnudo@users.noreply.github.com>
2024-08-30 14:09:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,29 +8,50 @@ function updateTime() {
 function fetchWeather() {
     const apiKey = 'SUA_API_KEY';
     const city = 'SUA_CIDADE';
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`;
+    const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&lang=pt`;
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            const weather = data.weather[0].description;
-            const temp = Math.round(data.main.temp);
+            const weather = data.current.condition.text;
+            const temp = Math.round(data.current.temp_c);
             document.getElementById('weather').textContent = `${weather}, ${temp}°C`;
-            updateImageBasedOnWeather(data.weather[0].main);
+            updateImageBasedOnWeather(data.current.condition.text);
         })
         .catch(error => console.error('Erro ao obter dados do clima:', error));
 }
 
-function updateImageBasedOnWeather(weatherCondition) {
-    const imageDisplay = document.getElementById('image-display');
-    const imageUrl = localStorage.getItem(`image-${weatherCondition}`);
+let images = [];
+let currentImageIndex = 0;
 
-    if (imageUrl) {
-        imageDisplay.innerHTML = `<img src="${imageUrl}" alt="Clima">`;
-    } else {
-        imageDisplay.innerHTML = `<img src="images/default.jpg" alt="Clima">`;
-    }
+function updateImageBasedOnWeather(weatherCondition) {
+    images = [
+        localStorage.getItem(`image-${weatherCondition}`) || 'images/default1.jpg',
+        localStorage.getItem(`image-${weatherCondition}`) || 'images/default2.jpg',
+        localStorage.getItem(`image-${weatherCondition}`) || 'images/default3.jpg',
+    ];
+    changeImage();
 }
 
+function changeImage() {
+    const imageDisplay = document.getElementById('image-display');
+    imageDisplay.innerHTML = `<img src="${images[currentImageIndex]}" alt="Clima">`;
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+}
+
+setInterval(changeImage, 5000); // Troca de imagem a cada 5 segundos
 setInterval(updateTime, 1000);
 fetchWeather();
+
+// Modo Tela Cheia
+const fullscreenBtn = document.getElementById('fullscreen-btn');
+fullscreenBtn.addEventListener('click', function() {
+    if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+    } else if (document.documentElement.webkitRequestFullscreen) { // Safari
+        document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) { // IE11
+        document.documentElement.msRequestFullscreen();
+    }
+    fullscreenBtn.style.display = 'none';
+});
